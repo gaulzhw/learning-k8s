9ed6592cc1c39363a5ed953c2b501af1c3e32670
gaulzhw <zhiwen.gaul@gmail.com>
2022-02-15 08:55:58 +0800

    reorganize code

--- code/sample-apiserver/pkg/controllers/shadow/shadow_controller.go
+++ code/sample-apiserver/pkg/controllers/shadow/shadow_controller.go
@@ -1,4 +1,4 @@
-package festival
+package shadow
 
 import (
 	"context"
--- code/sample-operator/hack/tools/tools.go
+++ code/sample-operator/hack/tools/tools.go
@@ -6,12 +6,7 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "k8s.io/code-generator/cmd/client-gen"
-	_ "k8s.io/code-generator/cmd/conversion-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"
-	_ "k8s.io/code-generator/cmd/informer-gen"
-	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/promo-tools/v3/cmd/kpromo"
