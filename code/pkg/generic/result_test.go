1521fbc1915b9635e1111c50d65b8af095ff5dc7
gzw <zhiwen.gaul@gmail.com>
2022-04-16 11:30:29 +0800

    generic result test

--- code/pkg/generic/result_test.go
+++ code/pkg/generic/result_test.go
@@ -5,12 +5,6 @@ import (
 )
 
 func TestUnwrap(t *testing.T) {
-	tests := struct[T any]{
-		Message: T,
-		Error: error,
-	}{
-		
-	}
 	(&Result[string]{Message: "test", Error: nil}).UnwrapOr("default")
 	(&Result[int]{Message: 1, Error: nil}).UnwrapOr(-1)
 }
