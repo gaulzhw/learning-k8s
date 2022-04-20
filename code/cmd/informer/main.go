49da3d6d2a164f6df2debb243341326f02c7f2a6
gzw <zhiwen.gaul@gmail.com>
2022-04-20 15:43:47 +0800

    update Makefile, support client-go gen

---
+++ code/cmd/informer/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/gaulzhw/learning-k8s/pkg/clientgo"
+)
+
+func main() {
+	clientgo.StartInformer()
+}
--- code/hack/tools/tools.go
+++ code/hack/tools/tools.go
@@ -6,6 +6,9 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
+	_ "k8s.io/code-generator/cmd/informer-gen"
+	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
