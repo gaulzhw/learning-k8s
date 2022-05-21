a32aea64829581b8723c6852914716bdc098cc68
gzw <zhiwen.gaul@gmail.com>
2022-05-21 15:57:42 +0800

    is palindrome

---
+++ algorithm/1-programming/is_palindrome.go
@@ -0,0 +1,62 @@
+package programming
+
+/*
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写
+说明：将空字符串定义为有效的回文串
+
+示例1：
+输入："A man, a plan, a canal: Panama"
+输出：true
+
+示例2：
+输入："race a car"
+输出：false
+*/
+
+// 时间复杂度O(n)，空间复杂度O(1)
+func isPalindrome(str string) bool {
+	i, j := 0, len(str)-1
+	for i < j {
+		if !isAlpha(str[i]) {
+			i++
+			continue
+		}
+		if !isAlpha(str[j]) {
+			j--
+			continue
+		}
+
+		if toLower(str[i]) != toLower(str[j]) {
+			return false
+		}
+
+		i++
+		j--
+	}
+	return true
+}
+
+// 判断是不是数字或者字母
+func isAlpha(b byte) bool {
+	if b >= 'a' && b <= 'z' {
+		return true
+	}
+	if b >= 'A' && b <= 'Z' {
+		return true
+	}
+	if b >= '0' && b <= '9' {
+		return true
+	}
+	return false
+}
+
+// 大写转小写
+func toLower(b byte) byte {
+	if b >= 'a' && b <= 'z' {
+		return b
+	}
+	if b >= '0' && b <= '9' {
+		return b
+	}
+	return b + 32
+}
---
+++ algorithm/1-programming/is_palindrome_test.go
@@ -0,0 +1,27 @@
+package programming
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsPalindromeString(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{
+			input:    "A man, a plan, a canal: Panama",
+			expected: true,
+		},
+		{
+			input:    "race a car",
+			expected: false,
+		},
+	}
+	for _, test := range tests {
+		got := isPalindrome(test.input)
+		assert.Equal(t, test.expected, got)
+	}
+}
