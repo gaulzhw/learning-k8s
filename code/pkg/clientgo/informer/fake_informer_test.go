b1fae15edc03744b00a022f086a96654e26a8ee0
gzw <zhiwen.gaul@gmail.com>
2022-06-09 00:01:34 +0800

    fake client for dynamic

--- code/pkg/clientgo/informer/fake_informer_test.go
+++ code/pkg/clientgo/informer/fake_informer_test.go
@@ -5,18 +5,23 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
 	"k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes/fake"
+	clientsetfake "k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/tools/cache"
 )
 
 func TestFakeInformer(t *testing.T) {
 	ctx := context.Background()
-
-	client := fake.NewSimpleClientset()
+	client := clientsetfake.NewSimpleClientset()
 
 	// We will create an informer that writes added pods to a channel.
 	pods := make(chan *corev1.Pod, 1)
@@ -52,4 +57,55 @@ func TestFakeInformer(t *testing.T) {
 	case <-time.After(wait.ForeverTestTimeout):
 		t.Error("Informer did not get the added pod")
 	}
+}
+
+func TestFakeDynamicInformer(t *testing.T) {
+	ctx := context.Background()
+	scheme := runtime.NewScheme()
+	corev1.AddToScheme(scheme)
+	client := dynamicfake.NewSimpleDynamicClient(scheme)
+
+	// prepare resource
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "test",
+		},
+	}
+	unstructuredPod, err := runtime.DefaultUnstructuredConverter.ToUnstructured(pod)
+	assert.NoError(t, err)
+
+	client.Resource(schema.GroupVersionResource{
+		Group:    corev1.SchemeGroupVersion.Group,
+		Version:  corev1.SchemeGroupVersion.Version,
+		Resource: "pods",
+	}).Namespace(pod.Namespace).Create(ctx, &unstructured.Unstructured{
+		Object: unstructuredPod,
+	}, metav1.CreateOptions{})
+
+	informerFactory := dynamicinformer.NewDynamicSharedInformerFactory(client, time.Second*30)
+	informerFactory.ForResource(schema.GroupVersionResource{
+		Group:    corev1.SchemeGroupVersion.Group,
+		Version:  corev1.SchemeGroupVersion.Version,
+		Resource: "pods",
+	}).Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			t.Log("add pod event")
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			t.Log("update pod event")
+		},
+		DeleteFunc: func(obj interface{}) {
+			t.Log("delete pod event")
+		},
+	})
+
+	stopper := make(chan struct{})
+	defer close(stopper)
+
+	informerFactory.Start(stopper)
+	informerFactory.WaitForCacheSync(stopper)
+	t.Log("synced")
+
+	<-stopper
 }
