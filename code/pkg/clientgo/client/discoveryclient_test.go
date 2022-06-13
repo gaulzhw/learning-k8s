a0c60631b7ff483d40ff7eb02040f39974cc8f91
gzw <zhiwen.gaul@gmail.com>
2022-06-13 16:48:39 +0800

    discovery client for cache

--- code/pkg/clientgo/client/discoveryclient_test.go
+++ code/pkg/clientgo/client/discoveryclient_test.go
@@ -4,10 +4,24 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/discovery/cached/memory"
 )
 
 func TestDiscoveryClient(t *testing.T) {
 	client, err := newFakeDiscoveryClient()
 	assert.NoError(t, err)
 	assert.NotNil(t, client)
+}
+
+func TestCachedDiscoveryClient(t *testing.T) {
+	client, err := newDiscoveryClient()
+	assert.NoError(t, err)
+	assert.NotNil(t, client)
+
+	cachedClient := memory.NewMemCacheClient(client)
+	groups, err := cachedClient.ServerGroups()
+	assert.NoError(t, err)
+	for _, group := range groups.Groups {
+		t.Log(group.Name)
+	}
 }
