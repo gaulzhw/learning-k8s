e2a105f444d53293fddd88fe79b7d7cc04e78d48
gzw <zhiwen.gaul@gmail.com>
2022-06-23 15:19:18 +0800

    add flag test

--- code/pkg/pflag/pflag_test.go
+++ code/pkg/pflag/pflag_test.go
@@ -2,21 +2,25 @@ package pflag
 
 import (
 	"testing"
+	"time"
 
 	"github.com/spf13/pflag"
 )
 
 var (
-	Names []string
+	names []string
+	lasts time.Duration
 )
 
 func TestPflags(t *testing.T) {
 	flags := pflag.FlagSet{}
-	flags.StringSliceVar(&Names, "names", []string{"*"}, "names joined with ,")
+	flags.StringSliceVar(&names, "names", []string{"*"}, "names joined with ,")
+	flags.DurationVar(&lasts, "lasts", 0, "time duration")
 
 	args := []string{
 		"--names=test1,test2",
+		"--lasts=3m",
 	}
 	flags.Parse(args)
-	t.Log(Names)
+	t.Log(names, lasts.Seconds())
 }
